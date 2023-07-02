9bb38b6ec45872f7b0f4949a7bdb17366d4a9eb8
Roberson Garcia <roberson.gar12@gmail.com>
2023-07-01 23:44:48 -0400

    xd

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { MapsApp } from './MapsApp.tsx'
 import mapboxgl from 'mapbox-gl'
 
-// mapboxgl.accessToken = 'pk.eyJ1Ijoicm9iZXJzb25nYXJjaWEiLCJhIjoiY2xocGlkMDE3MDlhNzNlbjA5Z2F3NWdrdCJ9.ZCo5X4EcOevB4T54blxOGQ';
-
 if(!navigator.geolocation) {
   alert('Geolocation is not supported by your browser')
   throw new Error('Geolocation is not supported by your browser')
